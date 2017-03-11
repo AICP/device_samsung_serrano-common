LineageOS/android_device_samsung_serrano-common

Nasty workaround to fix pthread issues in libc that breaks camera
Change-Id: I7b70b66c4fdd0be3e89cae14d9df2a9fb3217886

serrano-common.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
 
+# Shim for libc to fix camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/libshim_libc.so:system/lib/libshim_libc.so
+
 # GPS HAL
 PRODUCT_PACKAGES += \
     gps.msm8960

AICP/device_samsung_serrano-common

serrano-common.mk
@@ -90,6 +90,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
 
+# Shim for libc to fix camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/libshim_c.so:system/lib/libshim_c.so
+
 # GPS HAL
 PRODUCT_PACKAGES += \
     gps.msm8960